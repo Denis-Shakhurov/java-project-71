2daa01a97217aa939171966a765ecd1135c96869
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-08 18:19:36 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,25 @@
 package hexlet.code;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
 
-public class App {
+@Command(version = "Help demo v1.2.3",/*header = "%nAutomatic Help Demo%n",*/
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Runnable {
+    @Option(names = {"-h", "--help"}, usageHelp = true,
+            description = "Show this help message and exit.")
+    boolean usageHelpRequested;
+
+    @Option(names = {"-V", "--version"}, versionHelp = true,
+            description = "Print version information and exit.")
+    boolean versionHelpRequested;
+
+    @Override
+    public void run() {
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        CommandLine.run(new App(), System.err, args);
     }
 }
