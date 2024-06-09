42e72d4d33b40ecb8e09ac51fefafa40944b4110
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-09 07:53:20 +0700

    update App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,9 +2,13 @@ package hexlet.code;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
 
-@Command(version = "Help demo v1.2.3",/*header = "%nAutomatic Help Demo%n",*/
+import java.io.File;
+
+@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "Help demo v1.2.3", header = "%nAutomatic Help Demo%n",
         description = "Compares two configuration files and shows a difference.")
+
 public class App implements Runnable {
     @Option(names = {"-h", "--help"}, usageHelp = true,
             description = "Show this help message and exit.")
@@ -13,6 +17,14 @@ public class App implements Runnable {
     @Option(names = {"-V", "--version"}, versionHelp = true,
             description = "Print version information and exit.")
     boolean versionHelpRequested;
+
+    @Option(names = { "-f", "--format" }, paramLabel = "format", description = "output format [default: stylish]")
+    File archive;
+
+    @Parameters(paramLabel = "filePath1", description = "path to first file")
+    File filePath1;
+    @Parameters(paramLabel = "filePath2", description = "path to second file")
+    File filePath2;
 
     @Override
     public void run() {
