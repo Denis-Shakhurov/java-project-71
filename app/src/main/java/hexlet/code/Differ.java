2458c558546e9b5e3de0b5e3f7c0833901aeb627
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-10 10:03:36 +0700

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -12,16 +12,16 @@ import java.util.TreeMap;
 
 public class Differ {
     public static String generate(Path pathFile1, Path pathFile2) throws IOException {
-        Map<String, String> parseFile1 = getData(pathFile1);
-        Map<String, String> parseFile2 = getData(pathFile2);
+        Map<String, Object> parseFile1 = getData(pathFile1);
+        Map<String, Object> parseFile2 = getData(pathFile2);
 
         StringBuilder diff = new StringBuilder();
         diff.append("{\n");
 
-        for (Map.Entry<String, String> entry : parseFile1.entrySet()) {
+        for (Map.Entry<String, Object> entry : parseFile1.entrySet()) {
             String key = entry.getKey();
-            String value1 = entry.getValue();
-            String value2 = parseFile2.get(key);
+            String value1 = String.valueOf(entry.getValue());
+            String value2 = String.valueOf(parseFile2.get(key));
 
             if (value2 == null) {
                 diff.append(" - " + key + ": " + value1);
@@ -45,15 +45,12 @@ public class Differ {
         diff.append("}");
         return diff.toString();
     }
-    public static Map<String, String> getData(Path path) throws IOException {
+    public static Map<String, Object> getData(Path path) throws IOException {
         String absolutPath = String.valueOf(path);
         if (!path.isAbsolute()) {
             absolutPath = String.valueOf(path.toAbsolutePath());
         }
         ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, String> map = objectMapper.readValue(new File(absolutPath),
-                new TypeReference<TreeMap<String, String>>() { });
-
-        return map;
+        return objectMapper.readValue(new File(absolutPath), new TypeReference<TreeMap<String, Object>>() { });
     }
 }
