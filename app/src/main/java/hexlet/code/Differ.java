fcdedbec0827f88267dbf5056a66b024862273bb
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-11 10:10:06 +0700

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -15,41 +16,40 @@ public class Differ {
         Map<String, Object> parseFile1 = getData(pathFile1);
         Map<String, Object> parseFile2 = getData(pathFile2);
 
-        StringBuilder diff = new StringBuilder();
-        diff.append("{\n");
+        Map<String, Object> diff = new LinkedHashMap<>();
 
         for (Map.Entry<String, Object> entry : parseFile1.entrySet()) {
             String key = entry.getKey();
-            String value1 = String.valueOf(entry.getValue());
-            String value2 = String.valueOf(parseFile2.get(key));
-
-            if (value2 == null) {
-                diff.append(" - " + key + ": " + value1);
-                diff.append("\n");
-            } else if (!value1.equals(value2) && value2 != null) {
-                diff.append(" - " + key + ": " + value1);
-                diff.append("\n");
-                diff.append(" + " + key + ": " + value2);
-                diff.append("\n");
+            Object value1 = parseFile1.get(key);
+            Object value2 = parseFile2.get(key);
+            if (!parseFile2.containsKey(key)) {
+                diff.put("- " + key, value1);
             } else if (parseFile2.containsKey(key) && value1.equals(value2)) {
-                diff.append("   " + key + ": " + value1);
-                diff.append("\n");
+                diff.put("  " + key, value1);
+            } else if (parseFile2.containsKey(key) && !value1.equals(value2)) {
+                diff.put("- " + key, value1);
+                diff.put("+ " + key, value2);
             }
         }
         for (String key : parseFile2.keySet()) {
+            Object value = parseFile2.get(key);
             if (!parseFile1.containsKey(key)) {
-                diff.append(" + " + key + ": " + parseFile2.get(key));
-                diff.append("\n");
+                diff.put("+ " + key, value);
             }
         }
-        diff.append("}");
-        return diff.toString();
+        diff.entrySet().stream().sorted(Map.Entry.<String, Object>comparingByKey());
+        StringBuilder sb = new StringBuilder("{\n");
+        for (Map.Entry<String, Object> entry : diff.entrySet()) {
+            String key = entry.getKey();
+            Object value = diff.get(key);
+            sb.append(key + ": " + value + "\n");
+        }
+        sb.append("}");
+        return sb.toString();
     }
     public static Map<String, Object> getData(Path path) throws IOException {
-        String absolutPath = String.valueOf(path);
-        if (!path.isAbsolute()) {
-            absolutPath = String.valueOf(path.toAbsolutePath());
-        }
+        String absolutPath = String.valueOf(path.toAbsolutePath().normalize());
+
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readValue(new File(absolutPath), new TypeReference<TreeMap<String, Object>>() { });
     }
