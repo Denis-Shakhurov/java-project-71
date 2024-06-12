4897eb960145172335146d46adf9e13a30d3dda3
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-12 17:46:20 +0700

    update TestDiffer.java

--- app/src/test/java/hexlet/code/TestDiffer.java
+++ app/src/test/java/hexlet/code/TestDiffer.java
@@ -1,12 +1,16 @@
 package hexlet.code;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 public class TestDiffer {
     private Path getPath(String filename) {
@@ -17,15 +21,15 @@ public class TestDiffer {
         Path path = getPath(filename);
         return Files.readString(path);
     }
-    /*@Test
+    @Test
     public void testGetData() throws IOException {
-        File file = new File("src\\test\\resources\\file1.json");
+        String path = getPath("testFile1.json").toString();
         ObjectMapper objectMapper = new ObjectMapper();
-        Map<String, Object> map = objectMapper.readValue(file, new TypeReference<Map<String, Object>>() { });
+        Map<String, Object> map = objectMapper.readValue(path, new TypeReference<>() { });
 
         assertTrue(map.containsKey("timeout"));
         assertEquals(map.get("host"), "hexlet.io");
-    }*/
+    }
     @Test
     public void testGenerate() {
         String expected = null;
