59b0865df183761c06ff9c68ea796761b5af8fe2
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-13 13:13:01 +0700

    add Formatter.java

---
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -0,0 +1,24 @@
+package hexlet.code;
+
+import java.util.List;
+import java.util.Map;
+
+public class Formatter {
+    public static String stylish(List<Node> diff) {
+        StringBuilder sb = new StringBuilder(" {\n");
+        String space = " ";
+        for (Node node : diff) {
+            String status = node.getStatus();
+            String name = node.getName();
+            String value = String.valueOf(node.getValue());
+            switch (status) {
+                case "unchanged" : sb.append(space.repeat(4) + name + ": " + value + "\n"); break;
+                case "added" : sb.append(space.repeat(2) + "+ " + name + ": " + value + "\n"); break;
+                case "deleted" : sb.append(space.repeat(2) + "- " + name + ": " + value + "\n"); break;
+                default :
+                    System.out.println("Unknown status");
+            }
+        }
+        return sb.append(" }").toString();
+    }
+}
