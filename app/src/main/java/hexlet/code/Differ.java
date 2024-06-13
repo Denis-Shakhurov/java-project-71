8804bb27e22a27fb415c841bf1e245f2c1bf55c4
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-13 13:11:07 +0700

    update Differ.java

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -2,14 +2,11 @@ package hexlet.code;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 
 
 public class Differ {
-    public static String generate(Path pathFile1, Path pathFile2) {
+    public static String generate(Path pathFile1, Path pathFile2, String option) {
         Map<String, Object> parseFile1;
         Map<String, Object> parseFile2;
 
@@ -20,37 +17,41 @@ public class Differ {
             throw new RuntimeException(e);
         }
 
-        Set<String> keys = new TreeSet<>(parseFile1.keySet());
-        keys.addAll(parseFile2.keySet());
+        List<Node> diff = getMapDiffer(parseFile1, parseFile2);
+        String differ = "";
+        switch (option) {
+            case "stylish" : differ = Formatter.stylish(diff);
+            default :
+                System.out.println("Unknown format option");
+        }
 
-        Map<String, Object> diff = new LinkedHashMap<>();
+        return differ;
+    }
+    public static List<Node> getMapDiffer(Map<String, Object> mapParseFile1, Map<String, Object> mapParseFile2) {
+        Set<String> keys = new TreeSet<>(mapParseFile1.keySet());
+        keys.addAll(mapParseFile2.keySet());
+
+        List<Node> diff = new LinkedList<>();
         for (String key : keys) {
             Object value1;
             Object value2;
-            if (!parseFile2.containsKey(key)) {
-                value1 = parseFile1.get(key);
-                diff.put("- " + key, value1);
-            } else if (parseFile2.containsKey(key) && parseFile1.containsKey(key)) {
-                value1 = parseFile1.get(key);
-                value2 = parseFile2.get(key);
-                if (value1.equals(value2)) {
-                    diff.put("  " + key, value1);
+            if (!mapParseFile2.containsKey(key)) {
+                value1 = mapParseFile1.get(key);
+                diff.add(new Node(key, value1, "deleted"));
+            } else if (mapParseFile2.containsKey(key) && mapParseFile1.containsKey(key)) {
+                value1 = mapParseFile1.get(key);
+                value2 = mapParseFile2.get(key);
+                if (String.valueOf(value1).equals(String.valueOf(value2))) {
+                    diff.add(new Node(key, value1, "unchanged"));
                 } else {
-                    diff.put("- " + key, value1);
-                    diff.put("+ " + key, value2);
+                    diff.add(new Node(key, value1, "deleted"));
+                    diff.add(new Node(key, value2, "added"));
                 }
-            } else if (!parseFile1.containsKey(key)) {
-                value2 = parseFile2.get(key);
-                diff.put("+ " + key, value2);
+            } else if (!mapParseFile1.containsKey(key)) {
+                value2 = String.valueOf(mapParseFile2.get(key));
+                diff.add(new Node(key, value2, "added"));
             }
         }
-
-        StringBuilder sb = new StringBuilder("{\n");
-        for (String key : diff.keySet()) {
-            Object value = diff.get(key);
-            sb.append(key + ": " + value + "\n");
-        }
-        sb.append("}");
-        return sb.toString();
+        return diff;
     }
 }
