eae3b8c1981707c0444f21ac9391e33172db6564
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-15 20:00:23 +0700

    add Data.java Json.java

---
+++ app/src/main/java/hexlet/code/Data.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import java.util.*;
+
+public class Data {
+    public static List<Node> getData(Map<String, Object> mapParseFile1, Map<String, Object> mapParseFile2) {
+        Set<String> keys = new TreeSet<>(mapParseFile1.keySet());
+        keys.addAll(mapParseFile2.keySet());
+
+        List<Node> diff = new LinkedList<>();
+        for (String key : keys) {
+            Object value1;
+            Object value2;
+            if (!mapParseFile2.containsKey(key)) {
+                value1 = mapParseFile1.get(key);
+                diff.add(new Node(key, value1, "remove"));
+            } else if (mapParseFile2.containsKey(key) && mapParseFile1.containsKey(key)) {
+                value1 = mapParseFile1.get(key);
+                value2 = mapParseFile2.get(key);
+                if (String.valueOf(value1).equals(String.valueOf(value2))) {
+                    diff.add(new Node(key, value1, "unchanged"));
+                } else {
+                    diff.add(new Node(key, value1, "deleted"));
+                    diff.add(new Node(key, value2, "update"));
+                }
+            } else if (!mapParseFile1.containsKey(key)) {
+                value2 = mapParseFile2.get(key);
+                diff.add(new Node(key, value2, "added"));
+            }
+        }
+        return diff;
+    }
+}
---
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -0,0 +1,40 @@
+package hexlet.code.formatters;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import hexlet.code.Node;
+
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Json {
+    public static String createJson(List<Node> nodes) {
+        Map<String, Map<String, Object>> mapJson = new TreeMap<>();
+
+        mapJson.put("changed", new TreeMap<>());
+        mapJson.put("deleted", new TreeMap<>());
+        mapJson.put("added", new TreeMap<>());
+
+        ObjectMapper mapperJson = new ObjectMapper();
+
+        for (Node node : nodes) {
+            String key = node.getName();
+            Object value = node.getValue();
+            String status = node.getStatus();
+            switch (status) {
+                case "update" : mapJson.get("changed").put(key, value); break;
+                case "deleted", "remove" : mapJson.get("deleted").put(key, value); break;
+                case "added" : mapJson.get("added").put(key, value); break;
+                default : System.out.println("Unknown status"); break;
+            }
+        }
+        String fileJson = "";
+        try {
+            fileJson = mapperJson.writerWithDefaultPrettyPrinter().writeValueAsString(mapJson);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+        return fileJson;
+    }
+}
