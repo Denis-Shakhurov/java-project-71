9e0c2d2346d5f061d2782040ece9e4001f3629d9
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-28 13:33:24 +0700

    refactoring Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -11,15 +11,24 @@ public class Formatter {
     public static String formatToStylish(List<Map<String, Object>> differs) {
         return Stylish.convertToStylish(differs);
     }
+
     public static String formatToPlain(List<Map<String, Object>> differs) {
         return Plain.convertToPlain(differs);
     }
+
     public static String formatToJson(List<Map<String, Object>> differs) {
         return Json.createJson(differs);
     }
 
-
-    public static String format(String nameFormat) {
-        return nameFormat.toLowerCase().trim();
+    public static String format(List<Map<String, Object>> differs, String nameFormat) {
+        nameFormat.toLowerCase().trim();
+        String differ = "";
+        switch (nameFormat) {
+            case "stylish" : differ = Formatter.formatToStylish(differs); break;
+            case "plain" : differ = Formatter.formatToPlain(differs); break;
+            case "json" : differ = Formatter.formatToJson(differs); break;
+            default : System.out.println("Unknown format");
+        }
+        return differ;
     }
 }
