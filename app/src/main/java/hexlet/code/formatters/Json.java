db79827fe681cbee9ba635c15fcc49719fcd3709
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-06-28 13:34:10 +0700

    refactoring Json.java

--- app/src/main/java/hexlet/code/formatters/Json.java
+++ app/src/main/java/hexlet/code/formatters/Json.java
@@ -5,45 +5,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.List;
 import java.util.Map;
-import java.util.TreeMap;
 
 public class Json {
     public static String createJson(List<Map<String, Object>> differs) {
-        Map<String, Map<String, Object>> mapJson = new TreeMap<>();
-
-        mapJson.put("changed", new TreeMap<>());
-        mapJson.put("deleted", new TreeMap<>());
-        mapJson.put("added", new TreeMap<>());
-
-        ObjectMapper mapperJson = new ObjectMapper();
-        for (Map<String, Object> diff : differs) {
-            var keys = diff.keySet();
-            String[] keyAndStatus;
-            String status = "";
-            String keyMapJson = "";
-            Object value = null;
-            for (var key : keys) {
-                keyAndStatus = key.split(" ");
-                status = keyAndStatus[0];
-                keyMapJson = keyAndStatus[1];
-                value = diff.get(key);
-            }
-            if (!status.equals("unchanged")) {
-                switch (status) {
-                    case "update" : mapJson.get("changed").put(keyMapJson, value); break;
-                    case "deleted", "changed" : mapJson.get("deleted").put(keyMapJson, value); break;
-                    case "added" : mapJson.get("added").put(keyMapJson, value); break;
-                    default:
-                        System.out.println("Unknown status"); break;
-                }
-            }
-        }
-        String fileJson = "";
+        ObjectMapper mapper = new ObjectMapper();
+        String json = "";
         try {
-            fileJson = mapperJson.writerWithDefaultPrettyPrinter().writeValueAsString(mapJson);
+            json = mapper.writeValueAsString(differs);
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
-        return fileJson;
+        return json;
     }
 }
