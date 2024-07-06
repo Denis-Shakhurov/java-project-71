6789dfcda42c6fe020d7adea8d0ddb8efb88f44d
Denis_Shakhurov <disa.sdf86@gmail.com>
2024-07-06 09:55:56 +0700

    refactoring Formatter.java

--- app/src/main/java/hexlet/code/Formatter.java
+++ app/src/main/java/hexlet/code/Formatter.java
@@ -8,11 +8,11 @@ import java.util.List;
 import java.util.Map;
 
 public class Formatter {
-    private static String formatToStylish(List<Map<String, Object>> differs) {
+    private static String formatToStylish(List<Map<String, Object>> differs) throws Exception {
         return Stylish.convertToStylish(differs);
     }
 
-    private static String formatToPlain(List<Map<String, Object>> differs) {
+    private static String formatToPlain(List<Map<String, Object>> differs) throws Exception {
         return Plain.convertToPlain(differs);
     }
 
